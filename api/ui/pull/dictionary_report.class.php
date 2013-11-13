55edb9e7ad714d35440c782e66c890a7908b3481
Dean Inglis <inglisd@mcmaster.ca>
2013-11-13 16:10:07 -0500

    add download dictionary capability via report

---
+++ api/ui/pull/dictionary_report.class.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * dictionary_report.class.php
+ * 
+ * @author Dean Inglis <inglisd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace curry\ui\pull;
+use cenozo\lib, cenozo\log, curry\util;
+
+/**
+ * Mailout required report data.
+ * 
+ * @abstract
+ */
+class dictionary_report extends \cenozo\ui\pull\base_report
+{
+  /**
+   * Constructor
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @param array $args Pull arguments.
+   * @access public
+   */
+  public function __construct( $args )
+  {
+    parent::__construct( 'dictionary', $args );
+  }
+
+  /**
+   * Processes arguments, preparing them for the operation.
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @throws exception\notice
+   * @access protected
+   */
+  protected function prepare()
+  {
+    parent::prepare();
+
+    // check to see if a dictionary-specific template exists for this report
+    $db_dictionary = lib::create( 'database\dictionary', $this->get_argument( 'dictionary_id' ) );
+    $filename = sprintf( '%s/report/%s_%s.xls',
+                         DOC_PATH,
+                         $this->get_full_name(),
+                         $db_dictionary->name );
+    if( file_exists( $filename ) ) $this->report = lib::create( 'business\report', $filename );
+  }
+
+  /**
+   * Builds the report.
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @access protected
+   */
+  protected function build()
+  {
+    $this->report->set_orientation( 'landscape' );
+
+    $dictionary_class_name = lib::get_class_name( 'database\dictionary' );
+    $word_class_name = lib::get_class_name( 'database\word' );
+
+    $db_dictionary = lib::create( 'database\dictionary', $this->get_argument( 'dictionary_id' ) );
+    $word_count = $db_dictionary->get_word_count();
+
+    // loop through all dictionarys by region or site (based on breakdown), age group and gender
+    $word_mod = lib::create( 'database\modifier' );
+    $word_mod->where( 'word.dictionary_id', '=', $db_dictionary->id );
+    $word_mod->order( 'word.language' );
+    $data = array();
+    foreach( $word_class_name::select( $word_mod ) as $db_word )
+    {
+      $data[ $db_word->word ] = $db_word->language;
+    }
+
+    // create the content and header arrays using the data
+    $header = array( 'Word', 'Language'  );
+    $this->add_table( NULL, $header, $data );
+  }
+}
---
+++ api/ui/widget/dictionary_report.class.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * dictionary_report.class.php
+ * 
+ * @author Dean Inglis <inglisd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace curry\ui\widget;
+use cenozo\lib, cenozo\log;
+
+/**
+ * widget dictionary report
+ */
+class dictionary_report extends \cenozo\ui\widget\base_report
+{
+  /**
+   * Constructor
+   * 
+   * Defines all variables which need to be set for the associated template.
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @param array $args An associative array of arguments to be processed by the widget
+   * @access public
+   */
+  public function __construct( $args )
+  {
+    parent::__construct( 'dictionary', $args );
+  }
+
+  /**
+   * Processes arguments, preparing them for the operation.
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @throws exception\notice
+   * @access protected
+   */
+  protected function prepare()
+  {
+    parent::prepare();
+
+    $this->add_parameter( 'dictionary_id', 'enum', 'Dictionary' );
+    $this->set_variable( 'description',
+      'This report provides a list of dictionarys based on any of the provided restrictions.' );
+  }
+
+  /**
+   * Sets up the operation with any pre-execution instructions that may be necessary.
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @access protected
+   */
+  protected function setup()
+  {
+    parent::setup();
+
+    $dictionary_class_name = lib::get_class_name( 'database\dictionary' );
+
+    // create the enum lists
+    $dictionary_list = $dictionary_class_name::select();
+    $dictionaries = array();
+    foreach( $dictionary_list as $db_dictionary )
+      $dictionaries[$db_dictionary->id] = $db_dictionary->name;
+
+    $this->set_parameter( 'dictionary_id', key( $dictionaries ), true, $dictionaries );
+  }
+}
