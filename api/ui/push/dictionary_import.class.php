abe3dbf939f601d270c6044cf6c0b697d19f58f9
Dean Inglis <inglisd@mcmaster.ca>
2013-11-21 14:02:19 -0500

    complete initial dictionary import
    correctly implement the push functionality in push class
    using proceed button in twig

--- api/ui/push/dictionary_import.class.php
+++ api/ui/push/dictionary_import.class.php
@@ -25,63 +25,10 @@ class dictionary_import extends \cenozo\ui\push
   public function __construct( $args )
   {
     parent::__construct( 'dictionary', 'import', $args );
-    //call a log
-    log::debug( $this->arguments );
-    die();     
-  }
-
-
-  /** 
-   * Prepare the operation.
-   * 
-   * @author Dean Inglis <inglisd@mcmaster.ca>
-   * @throws exception\notice
-   * @access protected
-   */
-  protected function prepare()
-  {
-    parent::prepare();
-
-    if( 0 == $_SERVER['CONTENT_LENGTH'] )
-      throw lib::create( 'exception\notice',
-        'Tried to import participant data without a valid CSV file.',
-        __METHOD__ );
-    
-    $filename = $_SERVER['HTTP_X_FILENAME'];
-    $file_data = utf8_encode( file_get_contents( 'php://input' ) );
-
-    // now process the data
-    $row = 1;
-    $this->word_array = array();
-    foreach( preg_split( '/[\n\r]+/', $file_data ) as $line )
-    {
-      $values = str_getcsv( $line );
-
-      if(  1 == count( $values ) )
-      {
-        $word = strtolower( $values[0] );
-        if( count( str_word_count( $word, 1 ) ) > 1 ) 
-        {   
-          throw lib::create( 'exception\notice',
-            'Multiple word entries are not allowed.', __METHOD__ );
-        }
-        if( preg_match( '#[0-9]#', $word ) )
-        {   
-          throw lib::create( 'exception\notice',
-            'Not a valid word: numbers are not allowed.', __METHOD__ );
-        }
-        $this->word_array[] = $word;
-      }
-      $row++;
-    }
-    if( !empty( $this->word_array ) )
-    {
-      $this->word_array = array_unique( $this->word_array, SORT_STRING );
-    }
   }
 
   /**
-   * Validate the operation.
+   * This method executes the operation's purpose.
    * 
    * @author Dean Inglis <inglisd@mcmaster.ca>
    * @access protected
@@ -90,15 +37,15 @@ class dictionary_import extends \cenozo\ui\push
   {
     parent::execute();
     
-
-   // array_subtract
-    // get all the words in the target dictionary
-    // 
-    $dictionary_class_name = lib::get_class_name( 'database\dictionary' );
- 
-
+    $db_dictionary = lib::create( 'database\dictionary', $this->get_argument( 'dictionary_id' ) );
+    $word_list = $this->get_argument( 'word_list' );
+    foreach( $word_list as $word => $language )
+    {
+      $db_new_word = lib::create( 'database\word' );
+      $db_new_word->dictionary_id = $db_dictionary->id;
+      $db_new_word->word = $word;
+      $db_new_word->language = $language;    
+      $db_new_word->save();
+    }  
   }
-
-  protected $word_array;
-
 }
