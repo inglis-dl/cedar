9c8b52528632f2d233f037b15da5e2d219b75d1b
Dean Inglis <inglisd@mcmaster.ca>
2014-01-21 13:16:48 -0500

    WIP alpha numeric test transcribe
    finish basic functionality for alpha numeric
    test transcription

--- api/ui/push/test_entry_alpha_numeric_edit.class.php
+++ api/ui/push/test_entry_alpha_numeric_edit.class.php
@@ -12,7 +12,7 @@ use cenozo\lib, cenozo\log, cedar\util;
 /**
  * push: test_entry_alpha_numeric edit
  *
- * Edit an alpha numeric test entry.
+ * Edit a alpha_numeric test entry.
  */
 class test_entry_alpha_numeric_edit extends \cenozo\ui\push\base_edit
 {
@@ -26,4 +26,64 @@ class test_entry_alpha_numeric_edit extends \cenozo\ui\push\base_edit
   {
     parent::__construct( 'test_entry_alpha_numeric', $args );
   }
+
+  /** 
+   * Processes arguments, preparing them for the operation.
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @access protected
+   */
+  protected function prepare()
+  {
+    parent::prepare();
+
+    $columns = $this->get_argument( 'columns', array() );
+
+    // check to see if site_value is in the column list
+    if( array_key_exists( 'word_value', $columns ) ) 
+    {   
+      $this->word_value = $columns['word_value'];
+      unset( $this->arguments['columns']['word_value'] );
+    }   
+  }
+
+
+  /** 
+   * This method executes the operation's purpose.
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @access protected
+   */
+  protected function execute()
+  {
+    parent::execute();
+
+    $db_test_entry_alpha_numeric = $this->get_record();
+    $db_test_entry = $db_test_entry_alpha_numeric->get_test_entry();
+    $db_dictionary = $db_test_entry->get_test()->get_dictionary();
+
+    $language = $db_test_entry->get_assignment()->get_participant()->language;
+    $language = is_null( $language ) ? 'en' : $language;
+
+    // does the word candidate exist in the primary dictionary ?
+    $modifier = lib::create( 'database\modifier' );
+    $modifier->where( 'dictionary_id', '=', $db_dictionary->id );
+    $modifier->where( 'language', '=', $language );
+    $modifier->where( 'word', '=', $this->word_value );
+    $modifier->limit( 1 );
+    $word_class_name = lib::get_class_name( 'database\word' );
+    $db_word = $word_class_name::select( $modifier );
+    if( !empty( $db_word ) )
+    {
+      $db_test_entry_alpha_numeric->word_id = $db_word[0]->id;
+      $db_test_entry_alpha_numeric->save();
+    }
+  }
+
+  /**
+   * If a word_value is being set this member holds its new value.
+   * @var string $word
+   * @access protected
+   */
+  protected $word_value = NULL;
 }
--- api/ui/widget/test_entry_alpha_numeric_transcribe.class.php
+++ api/ui/widget/test_entry_alpha_numeric_transcribe.class.php
@@ -35,5 +35,63 @@ class test_entry_alpha_numeric_transcribe extends \cenozo\ui\widget
   protected function prepare()
   {
     parent::prepare();
+
+    // parent must be a test_entry_transcribe widget
+    if( is_null( $this->parent ) ) 
+      throw lib::create( 'exception\runtime', 'This class must have a parent', __METHOD__ );
+   
+    $db_test_entry = $this->parent->get_record();
+
+    $db_test = $db_test_entry->get_test();
+    $heading = $db_test->name . ' test entry form';
+
+    //TODO put this somewhere else
+    if( $db_test_entry->deferred )
+      $heading = $heading . ' NOTE: this test is currently deferred';
+
+    $this->set_heading( $heading );
+  }
+
+  /** 
+   * Sets up the operation with any pre-execution instructions that may be necessary.
+   * 
+   * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @throws exception\runtime
+   * @access protected
+   */
+  protected function setup()
+  {
+    parent::setup();
+
+    $db_test_entry = $this->parent->get_record();
+    $db_test = $db_test_entry->get_test();
+    $test_type_name = $db_test->get_test_type()->name;
+
+    if( $test_type_name != 'alpha_numeric' )
+      throw lib::create( 'exception\runtime',
+              'Widget requires test type to be alpha numeric, not ' .
+              $test_type_name, __METHOD__ );
+
+    $language = $db_test_entry->get_assignment()->get_participant()->language;
+    $language = is_null( $language ) ? 'en' : $language;
+
+    $modifier = lib::create( 'database\modifier' );
+    $modifier->order( 'rank' );
+    $entry_data = array();
+    //TODO add variable for dictionary lookup text completion
+    foreach( $db_test_entry->get_test_entry_alpha_numeric_list( $modifier ) as 
+      $db_test_entry_alpha_numeric )
+    {
+      $db_word = is_null(  $db_test_entry_alpha_numeric->word_id ) ? null :
+        lib::create( 'database\word', $db_test_entry_alpha_numeric->word_id );
+      $row = array(
+               'id' => $db_test_entry_alpha_numeric->id,
+               'rank' => $db_test_entry_alpha_numeric->rank,
+               'word_id' => is_null( $db_word ) ? '' :  $db_word->id,
+               'word' => is_null( $db_word ) ? '' :  $db_word->word );
+
+      $entry_data[] = $row;              
+    }
+    $this->set_variable( 'entry_data', $entry_data );
   }
 }
