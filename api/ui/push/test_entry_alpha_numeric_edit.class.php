19fee31afb5b40fa7b53c8966446f26e20ef18ea
Dean Inglis <inglisd@mcmaster.ca>
2014-02-07 13:51:36 -0500

    bug fix - getting it right
    add validation to the entry so that non letter or number
    inputs are rejected.  When rejected, clear the entry field
    and re-focus.

--- api/ui/push/test_entry_alpha_numeric_edit.class.php
+++ api/ui/push/test_entry_alpha_numeric_edit.class.php
@@ -27,26 +27,27 @@ class test_entry_alpha_numeric_edit extends \cenozo\ui\push\base_edit
     parent::__construct( 'test_entry_alpha_numeric', $args );
   }
 
-  /** 
-   * Processes arguments, preparing them for the operation.
+  /**
+   * Validate the operation.
    * 
    * @author Dean Inglis <inglisd@mcmaster.ca>
+   * @throws exception\notice
    * @access protected
    */
-  protected function prepare()
+  protected function validate()
   {
-    parent::prepare();
+    parent::validate();
 
-    $columns = $this->get_argument( 'columns', array() );
+    $columns = $this->get_argument( 'columns' );
 
-    // check to see if site_value is in the column list
     if( array_key_exists( 'word_value', $columns ) ) 
     {   
-      $this->word_value = $columns['word_value'];
-      unset( $this->arguments['columns']['word_value'] );
-    }   
+      if( !preg_match( '/^\d$/', $columns['word_value'] ) &&
+          !preg_match( '/^\pL$/', $columns['word_value'] ) )
+        throw lib::create( 'exception\notice',
+          'The word must be a letter or a number.', __METHOD__ );     
+    }
   }
-
 
   /** 
    * This method executes the operation's purpose.
@@ -64,12 +65,14 @@ class test_entry_alpha_numeric_edit extends \cenozo\ui\push\base_edit
 
     $language = $db_test_entry->get_assignment()->get_participant()->language;
     $language = is_null( $language ) ? 'en' : $language;
+    $columns = $this->get_argument( 'columns' );
+    $word_value = $columns['word_value'];
 
     // does the word candidate exist in the primary dictionary?
     $modifier = lib::create( 'database\modifier' );
     $modifier->where( 'dictionary_id', '=', $db_dictionary->id );
     $modifier->where( 'language', '=', $language );
-    $modifier->where( 'word', '=', $this->word_value );
+    $modifier->where( 'word', '=', $word_value );
     $modifier->limit( 1 );
     $word_class_name = lib::get_class_name( 'database\word' );
     $db_word = $word_class_name::select( $modifier );
@@ -79,17 +82,12 @@ class test_entry_alpha_numeric_edit extends \cenozo\ui\push\base_edit
     }
     else
     {
-      if( preg_match( '/^\d/', $this->word_value ) ||
-          preg_match( '/^\pL/', $this->word_value ) )
-      {
-        $db_new_word = lib::create( 'database\word' );
-        $db_new_word->dictionary_id = $db_dictionary->id;
-        $db_new_word->word = $this->word_value;
-        $db_new_word->language = $language;
-        $db_new_word->save();
-        $db_test_entry_alpha_numeric->word_id = static::db()->insert_id();
-      }
-      else return;
+      $db_new_word = lib::create( 'database\word' );
+      $db_new_word->dictionary_id = $db_dictionary->id;
+      $db_new_word->word = $word_value;
+      $db_new_word->language = $language;
+      $db_new_word->save();
+      $db_test_entry_alpha_numeric->word_id = static::db()->insert_id();
     }
 
     $db_test_entry_alpha_numeric->save();
@@ -104,11 +102,4 @@ class test_entry_alpha_numeric_edit extends \cenozo\ui\push\base_edit
     $completed = $test_entry_alpha_numeric_class_name::count( $modifier ) > 0 ? 1 : 0;
     $db_test_entry->update_status_fields( $completed );
   }
-
-  /**
-   * If a word_value is being set this member holds its new value.
-   * @var string $word
-   * @access protected
-   */
-  protected $word_value = NULL;
 }
