0d65743c0b5312eb2779a358faac1095c3c345f2
Dean Inglis <inglisd@mcmaster.ca>
2014-02-24 10:33:32 -0500

    missed a file during last commit
    enabling deletion of test_entry_*_ records

---
+++ sql/upgrade/0.1.2/role_has_operation.sql
@@ -0,0 +1,53 @@
+DROP PROCEDURE IF EXISTS patch_role_has_operation;
+DELIMITER //
+CREATE PROCEDURE patch_role_has_operation()
+  BEGIN
+
+    SET @cenozo = (
+      SELECT unique_constraint_schema
+      FROM information_schema.referential_constraints
+      WHERE constraint_schema = DATABASE()
+      AND constraint_name = "fk_role_has_operation_role_id" );
+
+      SELECT "Adding new operations to roles" AS "";
+
+      SET @sql = CONCAT(
+        "INSERT IGNORE INTO role_has_operation( role_id, operation_id ) ",
+        "SELECT role.id, operation.id FROM ", @cenozo, ".role, operation ",
+        "WHERE type = 'push' AND subject = 'test_entry_alpha_numeric' AND operation.name = 'delete' ",
+        "AND role.name IN ( 'administrator', 'typist' )" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+      SET @sql = CONCAT(
+        "INSERT IGNORE INTO role_has_operation( role_id, operation_id ) ",
+        "SELECT role.id, operation.id FROM ", @cenozo, ".role, operation ",
+        "WHERE type = 'push' AND subject = 'test_entry_classification' AND operation.name = 'delete' ",
+        "AND role.name IN ( 'administrator', 'typist' )" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+      SET @sql = CONCAT(
+        "INSERT IGNORE INTO role_has_operation( role_id, operation_id ) ",
+        "SELECT role.id, operation.id FROM ", @cenozo, ".role, operation ",
+        "WHERE type = 'push' AND subject = 'test_entry_confirmation' AND operation.name = 'delete' ",
+        "AND role.name IN ( 'administrator', 'typist' )" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+      SET @sql = CONCAT(
+        "INSERT IGNORE INTO role_has_operation( role_id, operation_id ) ",
+        "SELECT role.id, operation.id FROM ", @cenozo, ".role, operation ",
+        "WHERE type = 'push' AND subject = 'test_entry_ranked_word' AND operation.name = 'delete' ",
+        "AND role.name IN ( 'administrator', 'typist' )" );
+      PREPARE statement FROM @sql;
+      EXECUTE statement;
+      DEALLOCATE PREPARE statement;
+
+  END //
+DELIMITER ;
+CALL patch_role_has_operation();
+DROP PROCEDURE IF EXISTS patch_role_has_operation;
