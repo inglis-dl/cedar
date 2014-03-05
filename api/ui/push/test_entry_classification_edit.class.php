d6eb95518bd759c95305e4d6b01e9b6bfb01b918
Dean Inglis <inglisd@mcmaster.ca>
2014-03-05 15:08:49 -0500

    bug-fix: classification candidates to variant
    for FAS and AFT tests, ie., classification type tests,
    any word not classified as a primary or intrusion should be
    directly stored as a variant

--- api/ui/push/test_entry_classification_edit.class.php
+++ api/ui/push/test_entry_classification_edit.class.php
@@ -88,7 +88,7 @@ class test_entry_classification_edit extends \cenozo\ui\push\base_edit
     $classification = $data['classification'];
     $db_word = $data['word'];
 
-    if( $db_word !== NULL && $classification != 'candidate' )
+    if( $db_word !== NULL )
     {
       $record->word_id = $db_word->id;
       $record->word_candidate = NULL;
@@ -96,24 +96,26 @@ class test_entry_classification_edit extends \cenozo\ui\push\base_edit
     }
     else
     {
-      // the word isnt in the primary or intrusion dictionaries
+      // the word isnt in the primary, variant or intrusion dictionaries
+      $db_dictionary = NULL;
+
       if( $is_FAS )
       {
         $is_intrusion = false;
         if( preg_match( '/FAS (f words)/', $db_test->name ) &&
-            !( 0 === strpos( 'f', $record->word_candidate ) || 
-               0 === strpos( 'ph', $record->word_candidate ) ) )
+            !( 0 == strpos( 'f', $record->word_candidate ) || 
+               0 == strpos( 'ph', $record->word_candidate ) ) )
         {
           $is_intrusion = true;
         }
         else if( preg_match( '/FAS (a words)/', $db_test->name ) &&
-                 !( 0 === strpos( 'a', $record->word_candidate ) ) )
+                 !( 0 == strpos( 'a', $record->word_candidate ) ) )
         {
           $is_intrusion = true;
         }
         else if( preg_match( '/FAS (s words)/', $db_test->name ) &&
-                 !( 0 === strpos( 's', $record->word_candidate ) || 
-                    0 === strpos( 'c', $record->word_candidate ) ) )
+                 !( 0 == strpos( 's', $record->word_candidate ) || 
+                    0 == strpos( 'c', $record->word_candidate ) ) )
         {
           $is_intrusion = true;
         }          
@@ -122,26 +124,33 @@ class test_entry_classification_edit extends \cenozo\ui\push\base_edit
           //get the test's intrusion dictionary and add it as an intrusion
           $db_dictionary = $db_test->get_intrusion_dictionary();
           if( is_null( $db_dictionary ) ) 
-          {   
             throw lib::create( 'exception\notice',
               'Trying to add the word "'.  $record->word_candidate . '" to a non-existant ' .
               ' intrusion dictionary.  Assign an intrusion dictionary for the ' . 
               $db_test->name . ' test.', __METHOD__ );
-          }   
-          else
-          { 
-            $word_class_name = lib::get_class_name( 'database\word' );
-            $db_new_word = lib::create( 'database\word' );
-            $db_new_word->dictionary_id = $db_dictionary->id;
-            $db_new_word->word = $record->word_candidate;
-            $db_new_word->language = $language;
-            $db_new_word->save();
-            $record->word_id = $word_class_name::db()->insert_id();
-            $record->word_candidate = NULL;
-            $record->save();
-          }
         }  
       }
+
+      if( is_null( $db_dictionary ) )
+      {
+        //get the test's variant dictionary and add it as a variant
+        $db_dictionary = $db_test->get_variant_dictionary();
+        if( is_null( $db_dictionary ) ) 
+          throw lib::create( 'exception\notice',
+            'Trying to add the word "'.  $record->word_candidate . '" to a non-existant ' .
+            ' variant dictionary.  Assign a variant dictionary for the ' . 
+            $db_test->name . ' test.', __METHOD__ );
+      }
+
+      $word_class_name = lib::get_class_name( 'database\word' );
+      $db_new_word = lib::create( 'database\word' );
+      $db_new_word->dictionary_id = $db_dictionary->id;
+      $db_new_word->word = $record->word_candidate;
+      $db_new_word->language = $language;
+      $db_new_word->save();
+      $record->word_id = $word_class_name::db()->insert_id();
+      $record->word_candidate = NULL;
+      $record->save();
     }
 
     $modifier = lib::create( 'database\modifier' );
