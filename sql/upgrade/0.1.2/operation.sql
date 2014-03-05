627e4b887e05df186d0abb237d27b6a5ab2e63b1
Dean Inglis <inglisd@mcmaster.ca>
2014-03-05 10:49:32 -0500

    bug-fix: add missing sql patches to upgrade
    word list pull was missing from the patch

--- sql/upgrade/0.1.2/operation.sql
+++ sql/upgrade/0.1.2/operation.sql
@@ -21,6 +21,10 @@ primary, intrusion or variant." );
 INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "push", "test_entry_ranked_word", "delete", true, "Deletes an entry for an ranked word test type." );
 
+-- word
+INSERT IGNORE INTO operation( type, subject, name, restricted, description )
+VALUES( "pull", "word", "list", true, "Retrieves a list of words from a dictionary." );
+
 -- cenozo push
 INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "push", "self", "temporary_file", false,
