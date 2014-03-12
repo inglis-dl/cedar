23e23a77b1d98f429e03ea2076d27d5c7ced70d3
Dean Inglis <inglisd@mcmaster.ca>
2014-03-12 16:07:55 -0400

    bug-fix: remove redundant note operations
    cenozo already has all the necessary note operations:
    remove them from cedars setup sql scripts

--- sql/upgrade/0.1.2/operation.sql
+++ sql/upgrade/0.1.2/operation.sql
@@ -1,18 +1,5 @@
 SELECT "Adding new operations" AS "";
 
--- notes
-INSERT IGNORE INTO operation( type, subject, name, restricted, description )
-VALUES( "push", "note", "delete", true, "Removes a note from the system." );
-
-INSERT IGNORE INTO operation( type, subject, name, restricted, description )
-VALUES( "push", "note", "edit", true, "Edits the details of a note." );
-
-INSERT IGNORE INTO operation( type, subject, name, restricted, description )
-VALUES( "widget", "note", "list", false, "Displays a list of notes." );
-
-INSERT IGNORE INTO operation( type, subject, name, restricted, description )
-VALUES( "push", "note", "new", false, "Creates a new note." );
-
 -- test
 INSERT IGNORE INTO operation( type, subject, name, restricted, description )
 VALUES( "pull", "test", "classify_word", false, "Classifies a word candidate as either candidate, 
