bb318833500e557ced122d74a3451a8a2c543d32
Dean Inglis <inglisd@mcmaster.ca>
2014-05-22 10:54:58 -0400

    add cenozo region_site and jurisdiction
    - adminstrators can now add/edit/delete which sites a region is assigned to

--- sql/upgrade/0.1.4/patch_database.sql
+++ sql/upgrade/0.1.4/patch_database.sql
@@ -3,6 +3,10 @@
 SET AUTOCOMMIT=0;
 
 SOURCE test_entry.sql
+SOURCE operation.sql
+SOURCE role_has_operation.sql
+SOURCE jurisdiction.sql
+SOURCE service.sql
 
 -- after all SOURCES, update service version
 SOURCE update_version_number.sql;
