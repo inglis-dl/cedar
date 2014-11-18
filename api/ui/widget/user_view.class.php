c0b22652152d403c3852f4235fc4112c537a7d67
Dean Inglis <inglisd@mcmaster.ca>
2014-11-18 11:11:49 -0500

    remove unnecessary code

--- api/ui/widget/user_view.class.php
+++ api/ui/widget/user_view.class.php
@@ -59,7 +59,6 @@ class user_view extends \cenozo\ui\widget\user_view
   public function determine_cohort_count( $modifier = NULL )
   {
     $cohort_class_name = lib::get_class_name( 'database\cohort' );
-    $session = lib::create( 'business\session' );
     if( NULL == $modifier ) $modifier = lib::create( 'database\modifier' );
     $modifier->where( 'user_has_cohort.user_id', '=', $this->get_record()->id );
     return $cohort_class_name::count( $modifier );
@@ -76,7 +75,6 @@ class user_view extends \cenozo\ui\widget\user_view
   public function determine_cohort_list( $modifier = NULL )
   {
     $cohort_class_name = lib::get_class_name( 'database\cohort' );
-    $session = lib::create( 'business\session' );
     if( NULL == $modifier ) $modifier = lib::create( 'database\modifier' );
     $modifier->where( 'user_has_cohort.user_id', '=', $this->get_record()->id );
     return $cohort_class_name::select( $modifier );
